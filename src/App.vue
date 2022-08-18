a5a6dc5cde7fd861f9e4f2a96cc0e3f5b14c7dc4
ondrui <ondrui@yandex.ru>
2022-08-18 23:17:48 +0300

    add App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <div class="post">
+      <div><strong>Название:</strong> Пост о JavaScript</div>
+      <div><strong>Описание:</strong> JavaScript универсальный язык программирования</div>
+    </div>
+    <div class="post">
+      <div><strong>Название:</strong> Пост о JavaScript</div>
+      <div><strong>Описание:</strong> JavaScript универсальный язык программирования</div>
+    </div>
+    <div class="post">
+      <div><strong>Название:</strong> Пост о JavaScript</div>
+      <div><strong>Описание:</strong> JavaScript универсальный язык программирования</div>
+    </div>
+  </div>
 </template>
 
+<script>
+  export default {
+    data() {
+      return {
+        likes: 0,
+        dislikes: 0,
+      }
+    },
+    methods: {
+      addLikes() {
+        this.likes += 1;
+      },
+      addDislike() {
+        this.dislikes += 1;
+      }
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-nav {
-  padding: 30px;
-}
+  .post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+  }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
