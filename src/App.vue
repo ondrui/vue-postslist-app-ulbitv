bf30ba3cfa6e798bb4c919c03ee512ecdd07f394
ondrui <ondrui@yandex.ru>
2022-08-19 00:17:35 +0300

    created function add post & двухстороннее связывание в inputs

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
+  <div class="app">
+    <form @submit.prevent>
+      <h4>Создание поста</h4>
+      <input
+        v-bind:value="title"
+        @input="title = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Название"
+      />
+      <input
+        v-bind:value="body"
+        @input="body = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Описание"
+      />
+      <button class="btn" @click="createPost">Создать</button>
+    </form>
     <div class="post" v-bind:key="post.id" v-for="post in posts">
       <div><strong>Название:</strong> {{ post.title }}</div>
       <div><strong>Описание:</strong> {{ post.body }}</div>
@@ -8,34 +26,69 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        posts: [
-          {id: 1, title: 'JavaScript 1', body: 'Описание поста 1'},
-          {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
-          {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
-          {id: 4, title: 'JavaScript 4', body: 'Описание поста 4'},
-        ]
-      }
+export default {
+  data() {
+    return {
+      posts: [
+        { id: 1, title: 'JavaScript 1', body: 'Описание поста 1' },
+        { id: 2, title: 'JavaScript 2', body: 'Описание поста 2' },
+        { id: 3, title: 'JavaScript 3', body: 'Описание поста 3' },
+        { id: 4, title: 'JavaScript 4', body: 'Описание поста 4' },
+      ],
+      body: '',
+      title: '',
+    };
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      };
+      this.posts.push(newPost);
+      this.title = '';
+      this.body = '';
     },
-    methods: {
-
-    }
-  }
+  },
+};
 </script>
 
 <style>
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
-  .post {
-    padding: 15px;
-    border: 2px solid teal;
-    margin-top: 15px;
-  }
+.app {
+  padding: 20px;
+}
 
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
 </style>
