ee695741b49577384f4cd45c649c1adbe525e684
ondrui <ondrui@yandex.ru>
2022-08-25 23:49:24 +0300

    add animations to postList

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,25 +1,25 @@
 <template>
   <div v-if="posts.length > 0">
     <h3>Список пользователей</h3>
-    <PostItem
-      :key="post.id"
-      v-for="post in posts"
-      :post="post"
-      @remove="$emit('remove', post)"
-    />
+    <TransitionGroup name="post-list">
+      <PostItem
+        :key="post.id"
+        v-for="post in posts"
+        :post="post"
+        @remove="$emit('remove', post)"
+      />
+    </TransitionGroup>
   </div>
-  <h2 v-else style="color: red">
-    Список постов пуст!
-  </h2>
+  <h2 v-else style="color: red">Список постов пуст!</h2>
 </template>
 
 <script>
 import PostItem from './PostItem.vue';
 export default {
   components: {
-    PostItem
+    PostItem,
   },
-  emits:['remove'],
+  emits: ['remove'],
   props: {
     posts: {
       type: Array,
@@ -31,4 +31,18 @@ export default {
 </script>
 
 <style scoped>
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active ,
+.post-list-move{
+  transition: all 0.8s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
 </style>
